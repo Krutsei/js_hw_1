242b3f1dd0eb81117aad52a5fdff1975cae53f26
Andry Krutsei~ <krucay06y@gmail.com>
2023-06-12 10:48:24 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,24 @@
+const age = 17;
+console.log(age);
+
+const name = "Andriy";
+console.log(name);
+
+const isStudent = true;
+console.log(isStudent);
+
+const myString = "Never be afraid of what catches up with you.";
+console.log(myString);
+
+let myNumber = 6;
+myNumber += 10;
+console.log(myNumber);
+
+const myNull = null;
+console.log(myNull);
+
+const userName = prompt("Please enter your name.");
+console.log(userName);
+alert(`Hello`);
+
+console.log(alert("Carefully! Action is dangerous") + confirm("Please confirm the action"));
